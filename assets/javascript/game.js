479ff440e190147d211594b62f1dda570cf48ed0
BETeng <brian7.teng@gmail.com>
2019-03-31 00:00:00 -0500

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,45 +2,43 @@
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-//var playerGuess
-var guessed = ["a","b","c"];
-
+var guessesLeft = 10;
+var guessed = [];
 var LetterChoices = ["a", "b", "c","d", "e", "f","g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u","v", "w", "x","y", "z"];
 var playerGuess = ""
-// var scoreTracker = document.getElementById("scoreboard");
-// scoreTracker.textContent = "hello";
 var randomLetter = LetterChoices[Math.floor(Math.random()*LetterChoices.length)];
 console.log("computer random letter: ",randomLetter);
 
  document.onkeypress = function(event){
   playerGuess = event.key.toLowerCase();
-  
+  console.log("prev. inputs: ", guessed);
   console.log("YOU guessed: ",playerGuess);
- 
-  console.log("# of guesses: ",guessesLeft);
- 
-if(playerGuess == randomLetter){
-  console.log("You WON");
-}else if(guessed.includes(playerGuess)){
+
+if(guessed.includes(playerGuess)){
   console.log("you already chose this");
+
+}
+else if(playerGuess == randomLetter){
+  alert("You WON");
+
 }else if(playerGuess != randomLetter && guessesLeft > 0){
-   guessesLeft--;
+  guessesLeft--;
+  console.log("# of guesses: ",guessesLeft);
   console.log("keep trying");
+  guessed.push(playerGuess);
 }else{
   console.log("You LOSE!");
-
   
 }
-
+var scoreTracker = document.getElementById("scoreboard");
+scoreTracker.textContent = "You guessed: "+playerGuess;
+var scoreCounter = document.getElementById("counter");
+ scoreCounter.textContent = "You have: "+guessesLeft+" guesses remaining";
+ var pastGuess = document.getElementById("pastGuesses");
+ pastGuess.textContent = "You guessed: " +guessed;
 }
 
 
 
-
-//all different keys are put into an array
-
- 
-//run thru array and check for match
 
 
